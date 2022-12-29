2f5c0977471daebefb72b8797582492616f668aa
Joarez Ximenes <joarezximenesleitejunior@gmail.com>
2022-12-29 20:44:41 -0300

    feat: creating NavBar

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <div class="logo">
+      <img  src="../assets/cashforce_logo.svg" alt="Cashforce logo">
+    </div>
+    <div id="navlinks">
+      <router-link to="/">
+      <v-icon scale="1.5" name="fa-regular-handshake"/>
+      <p>Notas fiscais</p>
+    </router-link>
+    <router-link to="/about">
+      <v-icon speed="slow" scale="1.5" name="bi-gear-fill" animation="wrench" hover/>
+      <p>Configurações</p>
+    </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { OhVueIcon } from 'oh-vue-icons';
+
+export default {
+  name: 'NavBar',
+  components: {
+    'v-icon': OhVueIcon,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import '../assets/scss/NavBar.scss';
+</style>
