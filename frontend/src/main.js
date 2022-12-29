1a0fc1863715b376ab2811c13e93f4c755a4f5f9
Joarez Ximenes <joarezximenesleitejunior@gmail.com>
2022-12-29 20:44:11 -0300

    feat: implementing dinamic page title

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,14 @@
+import { OhVueIcon, addIcons } from 'oh-vue-icons';
 import { createApp } from 'vue';
+import { FaRegularHandshake, BiGearFill } from 'oh-vue-icons/icons';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+addIcons(FaRegularHandshake, BiGearFill);
+
+const app = createApp(App);
+app.component('v-icon', OhVueIcon);
+app.use(store);
+app.use(router);
+app.mount('#app');
