05b5df7bcc179f2b265df3bb1edabb17a676a9d2
Joarez Ximenes <joarezximenesleitejunior@gmail.com>
2023-01-01 23:46:38 -0300

    test: testing orders

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -4,5 +4,6 @@ describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('http://localhost:8080/');
     cy.contains('p', 'Visualize as notas fiscais que você tem.');
+    cy.contains('button', 'Dados do cedente');
   });
 });
